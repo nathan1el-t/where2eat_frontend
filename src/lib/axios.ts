a09c808b13417e90f87635b6d7803b9468a4fd52
nameless1130 <nathanielteo1130@gmail.com>
2025-07-17 15:19:27 +0800

    changed base url to connect remotely

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
-const BASE_URL = "http://localhost:8080/api";
+// const BASE_URL = "http://localhost:8080/api";
+const BASE_URL = "http://3.107.252.27:8080/api"
 
 const api = axios.create({
   baseURL: BASE_URL,
