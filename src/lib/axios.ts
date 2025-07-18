99fb729cd799c54239004a6d01dceaa87abfad78
nameless1130 <nathanielteo1130@gmail.com>
2025-07-18 11:45:54 +0800

    changed backend url

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 // const BASE_URL = "http://localhost:8080/api";
-const BASE_URL = "http://3.107.252.27:8080/api"
-
+// const BASE_URL = "http://3.107.252.27:8080/api"
+const BASE_URL = "https://where2eat-backend-ppik.onrender.com"
 const api = axios.create({
   baseURL: BASE_URL,
   withCredentials: true,
